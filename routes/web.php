1e43321d4541331a172c832d70c941cdbdd06816
caihong <694702948@qq.com>
2018-05-04 10:50:05 +0800

    Finish static pages

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
+
+Route::get('/test', function(){
+	return 'This is test.';
+});
