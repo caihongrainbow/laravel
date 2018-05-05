fafa7a78217abba265c5470b614c5401ab360551
vagrant <vagrant@homestead>
2018-05-05 03:10:04 +0000

    register

---
+++ resources/lang/zh-CN/validation.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'custom' => [
+	    'email' => [
+	        'required' => '邮箱地址1不能为空！',
+	    ],
+	],
+];
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,4 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
 
-Route::get('/test', function(){
-	return 'This is test.';
-});
+Route::resource('users', 'UsersController');
